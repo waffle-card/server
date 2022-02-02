a01c4c0607ff07fbe842807ba6d5f7a5f7e82510
younoah <kr_uno_10@naver.com>
2022-02-03 00:44:02 +0900

    feat: express 기본 미들웨어 추가

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'welcome to waffleCard!' });
