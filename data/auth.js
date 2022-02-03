fabd85dd43a25e33072cd58f7ec2bef6fa2fd56e
younoah <kr_uno_10@naver.com>
2022-02-03 17:26:58 +0900

    feat: 유저 모델 정의

---
+++ data/auth.js
@@ -0,0 +1,24 @@
+import Mongoose from 'mongoose';
+import { useVirtualId } from '../database/database.js';
+
+const userSchema = new Mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+useVirtualId(userSchema);
+
+const User = Mongoose.model('User', userSchema);
+
+export async function findByUsername(username) {
+  return User.findOne({ username });
+}
+
+export async function findById(id) {
+  return User.findById(id);
+}
+
+export async function createUser(user) {
+  return new User(user).save().then(data => data.id);
+}
